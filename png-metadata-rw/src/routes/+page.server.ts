04f6685b966a9681d596051d5f89acb562686746
T4D4 .icp <t4d4.icp@gmail.com>
2024-09-17 05:36:12 +0000

    delete +server.ts and update +page.server.ts-embedded and read text, return data to front

--- png-metadata-rw/src/routes/+page.server.ts
+++ png-metadata-rw/src/routes/+page.server.ts
@@ -1,11 +1,17 @@
 import { writeFileSync } from "node:fs";
-import { error, fail } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import fs from "node:fs";
 import { console } from "node:inspector";
+import { error, fail } from "@sveltejs/kit";
+import { decodeSync, encodeSync } from "png-chunk-itxt";
+import encode from "png-chunks-encode";
+// packages to use for read/write iTxt chunk
+import extract from "png-chunks-extract";
+import type { PageServerLoad } from "./$types";
 
 export const actions = {
 	default: async ({ request }) => {
 		const formData = Object.fromEntries(await request.formData());
+		// console.log(formData);
 
 		if (
 			!(formData.fileToUpload as File).name ||
@@ -16,7 +22,7 @@ export const actions = {
 				message: "ファイルをアップロードしてください",
 			});
 		}
-		const { fileToUpload } = formData as { fileToUpload: File };
+		const { fileToUpload, text } = formData as { fileToUpload: File, text: string };
 
 		// Write the to the static folder
 		writeFileSync(
@@ -24,8 +30,55 @@ export const actions = {
 			Buffer.from(await fileToUpload.arrayBuffer()),
 		);
 
+		const buffer = fs.readFileSync(`static/${fileToUpload.name}`);
+		const chunks = extract(buffer);
+
+		let embeddedText = "";
+		// フォームに入力されたテキストがある場合iTxtチャンクに書き込みをする
+		if (text) {
+			// iTxt chunkへ書きこむデータの整形
+			const write_iTxtChunk = {
+				name: "iTXt",
+				data: encodeSync({
+					keyword: "metadata",
+					compressionFlag: false,
+					compressionMethod: 0,
+					languageTag: "",
+					translatedKeyword: "",
+					text: text, // TODO フォームで入力した内容をココに入れる
+				}),
+			};
+			// 最初のIDATチャンクの前にiTXtチャンクを挿入する。
+			chunks.splice(
+				chunks.findIndex((p) => p.name === "IDAT"),
+				0,
+				write_iTxtChunk,
+			);
+
+			const newFilePath = `static/embedded_${fileToUpload.name}`;
+			fs.writeFileSync(newFilePath, Buffer.from(encode(chunks)));
+
+			return {
+				success: true,
+				embeddedText:  text,
+				downloadLink: `static/emvedded_${fileToUpload.name}`
+			};
+		} else {
+
+		// iTxt chunkから読み取る
+		const read_iTxtChunk = chunks.find((c) => c.name === "iTXt");
+
+		if (read_iTxtChunk !== undefined) {
+			const iTxtData = decodeSync(read_iTxtChunk.data);
+			embeddedText = iTxtData.text;
+		} else {
+			console.error("iTxtChunk is  undefined");
+		}
+
 		return {
 			success: true,
+			embeddedText:  embeddedText,
 		};
+	}
 	},
 };
